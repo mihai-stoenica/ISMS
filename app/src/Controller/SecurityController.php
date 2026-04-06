860edec1b5dd92f877e3d513209258eb00f0fd03
Stoenica Mihai <stoenica.mihai2016@gmail.com>
2026-04-06 22:51:48 +0300

    implemented basic user authentication

---
+++ app/src/Controller/SecurityController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(
+        AuthenticationUtils $authenticationUtils
+    ): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        Security $security
+    ) : Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            $user->setRoles(['ROLE_PENDING']);
+            $user->setIsAccepted(false);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $security->login($user, 'form_login', 'main');
+        }
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form
+        ]);
+    }
+}
---
+++ app/src/Form/RegistrationType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'Full Name', 'class' => 'input w-full']
+            ])
+            ->add('email')
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'error_bubbling' => true,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => ['class' => 'input w-full', 'placeholder' => 'Password'],
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => ['class' => 'input w-full', 'placeholder' => 'Repeat Password'],
+                ],
+                'invalid_message' => 'The password fields must match.',
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank(
+                        message: 'The password cannot be blank.',
+                    ),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Your password must be at least {{ limit }} characters long.',
+                    ),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
