754a153e5f572d0d2e5e31eaf9f72cb4bb78f328
Stoenica Mihai <stoenica.mihai2016@gmail.com>
2026-04-28 16:27:33 +0300

    added validation for task locations

--- app/src/Service/TaskService.php
+++ app/src/Service/TaskService.php
@@ -2,8 +2,10 @@
 
 namespace App\Service;
 
+use App\Entity\Product;
 use App\Entity\Task;
 use App\Entity\User;
+use App\Enum\Location;
 use App\Enum\TaskStatus;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -40,6 +42,24 @@ class TaskService
         $this->entityManager->flush();
     }
 
+    private function isLocationEmpty(Location $location) : bool
+    {
+        $productCount = $this->entityManager
+            ->getRepository(Product::class)
+            ->count([
+                'location' => $location
+            ]);
+
+        $taskCount = $this->entityManager
+            ->getRepository(Task::class)
+            ->count([
+                'destination' => $location,
+                'status' => [TaskStatus::PENDING, TaskStatus::ASSIGNED],
+            ]);
+
+        return ($productCount == 0 && $taskCount == 0);
+    }
+
     public function getErrorMessage(Task $newTask) : ?string
     {
         $productTasks = $newTask->getProduct()->getTasks();
@@ -59,6 +79,10 @@ class TaskService
 
         if(!in_array('ROLE_STAFF', $newTask->getEmployee()->getRoles())) {
             return "You can only assign tasks to a staff member";
+        }
+
+        if(!$this->isLocationEmpty($newTask->getDestination())) {
+            return "This location is already in use";
         }
 
         return null;
